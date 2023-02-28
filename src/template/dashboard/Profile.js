f4509db9867c727efdf93989c579782ca68b3ccc
Luis Osnet <luisosnet@gmail.com>
2023-02-27 21:02:48 -0600

    add logout

--- src/template/dashboard/Profile.js
+++ src/template/dashboard/Profile.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import logo from '../../assets/img/mind.png';
-
+import { hostUrl } from '../../services/apirest';
+import axios from "axios";
 class Profile extends React.Component {
+  delete = async e=> {
+		let url = hostUrl + 'api/v1/logout';
+
+		axios.delete(url, {
+			headers: {
+				'Authorization': `${localStorage.getItem("session")}`
+			}
+		})
+		.then(response =>{
+      window.location.href="/login";
+		})
+	}
   render(){
     return(
       <header className="header_area">
         <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-              <a className="navbar-brand" href="/landing"><img src={logo} alt="Logo" /></a>
-              {/* <ul className="nav">
-                <li className="nav-item">
-                  <a className="nav-link active" href="/usuarios">Usuarios</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/cuentas">Cuentas</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/equipos">Equipos</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/movimientos">Movimientos</a>
-                </li>
-              </ul> */}
+              <a className="navbar-brand" href="/"><img src={logo} alt="Logo" /></a>
             </div>
+            <button type="button" className="btn btn-info float-right" onClick={()=>this.delete()}>Salir</button>
           </nav>      
-        </div>
+        </div>       
       </header>
     );
   }
