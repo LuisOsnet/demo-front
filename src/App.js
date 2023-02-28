69d947091a31eda91bb467a6aad0fdcfa759e31c
Luis Osnet <luisosnet@gmail.com>
2023-02-27 20:16:09 -0600

    add profile

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import './assets/css/App.css'
 import 'bootstrap/dist/css/bootstrap.css'
-// import Signin from './components/Signin'
-// import Usuarios from "./components/Usuarios";
-// import Editar from "./components/Editar";
-
-
+// Basics
 import Landing from "./components/landing/Landing";
 import Login from "./components/login/Login";
+// Users
 import Users from "./components/users/Users";
 import User from "./components/users/User";
 import New from "./components/users/New";
+import Profile from "./components/users/Profile";
 // accounts
 import Accounts from "./components/accounts/Accounts";
 import Account from "./components/accounts/Account";
@@ -38,6 +35,7 @@ function App() {
             <Route path="/usuarios" exact render ={ props=> ( <Users {...props} /> ) }></Route>
             <Route path="/usuario/:id" exact render ={ props=> ( <User {...props} /> ) }></Route>
             <Route path="/nuevo-usuario" exact render ={ props=> ( <New {...props} /> ) }></Route>
+            <Route path="/perfil/:id" exact render ={ props=> ( <Profile {...props} /> ) }></Route>
 
             <Route path="/cuentas" exact render ={ props=> ( <Accounts {...props} /> ) }></Route>
             <Route path="/cuenta/:id" exact render ={ props=> ( <Account {...props} /> ) }></Route>
